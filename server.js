093b3a3c29cce61d151972cd3330321837599047
JessieMFox <95918500+JessieMFox@users.noreply.github.com>
2022-03-30 22:45:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-var exec = require("child_process").exec;
-exec("chmod +x ./pp", function (err, stdout, stderr) {
-  if (err) {
-    console.log("error:" + stderr);
-  } else {
-    console.log(stdout);
-  }
-});
-exec("./pp --login renqer@outlook.com", function (err, stdout, stderr) {
-  if (err) {
-    console.log("error:" + stderr);
-  } else {
-    console.log(stdout);
-  }
-});
 var http = require("http");
 var app_port = process.env.app_port || 8080;
 var app_host = process.env.app_host || "127.0.0.1";
@@ -25,18 +10,3 @@ http
   })
   .listen(app_port);
 console.log("Web server running at http://" + app_host + ":" + app_port);
-
-exec(
-  "pwd",
-  {
-    cwd: '/',
-  },
-  function (err, stdout, stderr) {
-    if (err) {
-      console.log(err);
-      console.log("error:" + stderr);
-    } else {
-      console.log(stdout);
-    }
-  }
-);
